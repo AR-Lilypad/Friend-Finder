bd2827aa83867034ea38fb5a6236952dc9300dab
Angie Roach <angieroach4@gmail.com>
2020-01-18 22:46:08 -0500

    establish routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,34 +1,39 @@
 let friends = [
-    {
-        name: "Chopper",
-        photo: "https://unsplash.com/photos/Sac6Ri0Lhrc",
-        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
-    },
-    {
-        name: "Hipster",
-        photo: "https://unsplash.com/photos/Sac6Ri0Lhrc",
-        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
-    },
-    {
-        name: "Millenial",
-        photo: "https://unsplash.com/photos/Sac6Ri0Lhrc",
-        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
-    },
-    {
-        name: "Chopper",
-        photo: "https://unsplash.com/photos/Sac6Ri0Lhrc",
-        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
-    },
-    {
-        name: "Chopper",
-        photo: "https://unsplash.com/photos/Sac6Ri0Lhrc",
-        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
-    },
-    {
-        name: "Chopper",
-        photo: "https://unsplash.com/photos/Sac6Ri0Lhrc",
-        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
-    }
+  {
+    name: "Chopper",
+    photo:
+      "https://www.telegraph.co.uk/content/dam/news/2016/06/20/101167774_A_biker_shows_his_12-year-old_cat_Chiquinho_-which_always_rides_with_him_on_his_motorbike-_trans_NvBQzQNjv4BqpVlberWd9EgFPZtcLiMQf1X_50byq9Ah3wJAV0YS_Ms.jpg?imwidth=900",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: "Hipster",
+    photo: "https://unsplash.com/photos/yMSecCHsIBc",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: "Gen Z",
+    photo:
+      "https://ih0.redbubble.net/image.512963578.8968/flat,750x,075,f-pad,750x1000,f8f8f8.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: "Millenial",
+    photo:
+      "https://i0.wp.com/digiday.com/wp-content/uploads/2015/06/millennial-reader.png?zoom=2&resize=1440%2C442",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: "Gen X",
+    photo:
+      "https://fydn.imgix.net/m%2Fpod-artwork%2Fpool%2Fc791361064f765fa1facc6f2ad93b3d5-cat-tupac-art-print-P1.jpg?ar=1%3A1&auto=format%2Ccompress&crop=top&fit=crop&q=75&w=960",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: "Boomer",
+    photo:
+      "http://1.bp.blogspot.com/-qDz04Xmxz1Y/VPHUsEBAOII/AAAAAAAAInE/fPPT9BOF9NU/s1600/Screenshot%2B2015-02-28%2B09.39.21.png",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  }
 ];
 
 module.exports = friends;
